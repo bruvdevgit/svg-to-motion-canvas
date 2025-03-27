a62012a61da1b91739a29524c897ca552e1e5fb7
Haxwell <thehaxwell@proton.me>
2025-03-27 13:31:09 +0200

    added rough UnresolvedExpression utility

---
+++ packages/translator/src/utilities/UnresolvedExpression.ts
@@ -0,0 +1,32 @@
+export type ValueOrExpression = Expression | number;
+
+// This class exists because the result of this
+// library is code that will be processed on a higher
+// layer of execution.
+//
+// This class then serves the purpose of generating
+// in the output code an expression like 1+1 
+// instead of its resolution 2.
+//
+// The purpose of outputting unresolved expressions 
+// even though they will be computed the same is to
+// give users of this library a hint about where values
+// are coming from.
+export interface Expression {
+  getExpression(): string;
+  add(expression: Expression): this;
+}
+
+export class _Expression {
+  constructor(public expression: string) { }
+
+  getExpression(): string {
+    return this.expression;
+  }
+}
+
+export type InitExpressionFn
+  = (expression: string) => Expression;
+
+export const initExpression: InitExpressionFn
+  = (expression: string) => new _Expression(expression);
